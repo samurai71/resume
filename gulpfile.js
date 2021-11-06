f13a0645dede7716fb6b0eb68ef1ced34a996f16
Mark Landeryou <marklanderyou@hotmail.com>
2021-11-06 08:22:11 -0400

    trying to get gulp to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
+/* eslint-disable no-undef */
 const { src, dest, parallel, series, watch } = require("gulp");
 
 // Load plugins
 
-//const uglify = require("gulp-uglify");
+const uglify = require("gulp-uglify");
 const rename = require("gulp-rename");
 //const sass = require("gulp-sass")(require("sass"));
-//const autoprefixer = require("gulp-autoprefixer");
-//const cssnano = require("gulp-cssnano");
+const autoprefixer = require("gulp-autoprefixer");
+const cssnano = require("gulp-cssnano");
 const concat = require("gulp-concat");
 const clean = require("gulp-clean");
 //const imagemin = require("gulp-imagemin");
-//const cwebp = require('gulp-cwebp');
-//const imageminMozjpeg = require('imagemin-mozjpeg');
+//const newer = require("gulp-newer");
+//const cwebp = require("gulp-cwebp");
+//const imageminMozjpeg = require("imagemin-mozjpeg");
 const changed = require("gulp-changed");
 const stripDebug = require("gulp-strip-debug");
 const browsersync = require("browser-sync").create();
@@ -26,14 +28,14 @@ function clear() {
 
 // JS function
 
-/* function js() {
+function js() {
   const source = "./src/js/*.js";
 
   return (
     src(source)
       .pipe(changed(source))
-      .pipe(concat("bundle.js"))
-      //.pipe(uglify())
+      //.pipe(concat("bundle.js"))
+      .pipe(uglify())
       .pipe(
         rename({
           extname: ".min.js"
@@ -43,7 +45,7 @@ function clear() {
       .pipe(dest("./dist/js/"))
       .pipe(browsersync.stream())
   );
-} */
+}
 
 // html function
 function html() {
@@ -59,24 +61,30 @@ function html() {
 
 function css() {
   const source = "./src/css/*.css";
+  //const postcss = require("gulp-postcss");
+  const sourcemaps = require("gulp-sourcemaps");
 
   return (
     src(source)
       .pipe(changed(source))
       //.pipe(sass())
-      /*.pipe(
-      autoprefixer({
-        overrideBrowserslist: ['last 2 versions'],
-        cascade: false,
-      })
-    )*/
+      .pipe(
+        autoprefixer({
+          overrideBrowserslist: ["last 2 versions"],
+          cascade: false
+        })
+      )
       .pipe(concat("styles.css"))
       .pipe(
         rename({
           extname: ".min.css"
         })
       )
-      //.pipe(cssnano())
+      .pipe(cssnano())
+      .pipe(sourcemaps.init())
+      // .pipe(postcss([require("autoprefixer"))]))
+      .pipe(sourcemaps.write("."))
+      //.pipe(gulp.dest("build/"))
       .pipe(dest("./dist/css/"))
       .pipe(browsersync.stream())
   );
@@ -87,7 +95,7 @@ function css() {
 function img() {
   return (
     src("./src/images/*")
-      /* .pipe(
+      /*.pipe(
         imagemin([
           imagemin.gifsicle({ interlaced: true }),
           //imagemin.mozjpeg({ quality: 75, progressive: true }),
@@ -96,17 +104,39 @@ function img() {
             plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
           })
         ])
-      ) */
-      //.pipe(cwebp())
-      .pipe(dest("./dist/images"))
+      )*/
+      // .pipe(cwebp())
+      .pipe(dest("./dist/images/"))
   );
 }
+
+// function img() {
+//   return gulp
+//     .src("./src/images/**/*")
+//     .pipe(newer("./dist/images/"))
+//     .pipe(
+//       imagemin([
+//         imagemin.gifsicle({ interlaced: true }),
+//         imagemin.jpegtran({ progressive: true }),
+//         imagemin.optipng({ optimizationLevel: 5 }),
+//         imagemin.svgo({
+//           plugins: [
+//             {
+//               removeViewBox: false,
+//               collapseGroups: true
+//             }
+//           ]
+//         })
+//       ])
+//     )
+//     .pipe(gulp.dest("./dist/images/"));
+// }
 
 // Watch files
 
 function watchFiles() {
   watch("./src/css/*", css);
-  //watch("./src/js/*", js);
+  watch("./src/js/*", js);
   watch("./src/images/*", img);
   watch("./src/*.html", html);
 }
@@ -125,4 +155,4 @@ function browserSync() {
 // Tasks to define the execution of the functions simultaneously or in series
 
 exports.watch = parallel(watchFiles, browserSync);
-exports.default = series(clear, parallel(css, img, html));
+exports.default = series(clear, parallel(js, css, img, html));
